c47d13539a18f8a1b80d24aaef4ecf776caa2344
Travenesia <travenesia@gmail.com>
2022-04-24 10:28:27 +0700

    .

---
+++ precache-manifest.0acb96981ac99ca2617c1c7c9930f2c3.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "48defcc7414ff7b68380",
+    "url": "/static/js/main.48defcc7.chunk.js"
+  },
+  {
+    "revision": "a0ea8e462c03a622087e",
+    "url": "/static/css/1.7ba49898.chunk.css"
+  },
+  {
+    "revision": "a0ea8e462c03a622087e",
+    "url": "/static/js/1.a0ea8e46.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dbaf04e322f47dfd30f96dd9bcc74dc2",
+    "url": "/static/media/Arrow.dbaf04e3.svg"
+  },
+  {
+    "revision": "90e7b017d43e5272e759e306adce59a3",
+    "url": "/static/media/aethernityvideo.90e7b017.mp4"
+  },
+  {
+    "revision": "b425dd11225083bc427ba764a6d550a9",
+    "url": "/static/media/Rounded-Text-Black.b425dd11.png"
+  },
+  {
+    "revision": "a667fa0527980fdb418f0e15e3615db2",
+    "url": "/static/media/1.a667fa05.svg"
+  },
+  {
+    "revision": "6186511d85a9aae385228c758db67f6b",
+    "url": "/static/media/2.6186511d.svg"
+  },
+  {
+    "revision": "f8ddefea6fcddcd03b6290c679d19784",
+    "url": "/static/media/3.f8ddefea.svg"
+  },
+  {
+    "revision": "5878e381aff7f3928d6c75896078a91c",
+    "url": "/static/media/4.5878e381.svg"
+  },
+  {
+    "revision": "b33ddfb00c2435d2721480b4ec6d7163",
+    "url": "/static/media/5.b33ddfb0.svg"
+  },
+  {
+    "revision": "aa1e80420d29dfaf72d0c11cc4f3de4a",
+    "url": "/static/media/6.aa1e8042.svg"
+  },
+  {
+    "revision": "ce9ba89f5333fc878f3dcecae0bbe534",
+    "url": "/static/media/7.ce9ba89f.svg"
+  },
+  {
+    "revision": "122dbe29e5717302a30280e16446035e",
+    "url": "/static/media/8.122dbe29.svg"
+  },
+  {
+    "revision": "f0fd083d714db9343e649e78014ce1c6",
+    "url": "/static/media/9.f0fd083d.svg"
+  },
+  {
+    "revision": "c27e6faf62f19a59899c218da5f84a18",
+    "url": "/static/media/10.c27e6faf.svg"
+  },
+  {
+    "revision": "329173288e209796c611a5c57f33e6c9",
+    "url": "/static/media/travenesia.32917328.png"
+  },
+  {
+    "revision": "8a2ab7a53b2dcdfe512572224dfdb352",
+    "url": "/static/media/a.8a2ab7a5.png"
+  },
+  {
+    "revision": "1d145c46c793b6d955509614318b1849",
+    "url": "/static/media/b.1d145c46.png"
+  },
+  {
+    "revision": "e176a0bfb1edf75e47aad8897f960ae4",
+    "url": "/static/media/c.e176a0bf.png"
+  },
+  {
+    "revision": "3433a14a5c0b4075f233b0ff750e4cb0",
+    "url": "/static/media/d.3433a14a.png"
+  },
+  {
+    "revision": "afb2e0d261332e4added068a304c0f8d",
+    "url": "/static/media/e.afb2e0d2.png"
+  },
+  {
+    "revision": "816ca97e95e5a61d6b9a951a7c2184fd",
+    "url": "/static/media/akaya-telivigala-telugu-400-normal.816ca97e.woff2"
+  },
+  {
+    "revision": "7f56574a8493be13da61061b45742a8e",
+    "url": "/static/media/akaya-telivigala-all-400-normal.7f56574a.woff"
+  },
+  {
+    "revision": "23de3ed1a64ec7291ea78365bdda294a",
+    "url": "/static/media/akaya-telivigala-latin-400-normal.23de3ed1.woff2"
+  },
+  {
+    "revision": "6a3055dd022717f38098e0f1f93daa4a",
+    "url": "/static/media/akaya-telivigala-latin-ext-400-normal.6a3055dd.woff2"
+  },
+  {
+    "revision": "5919e76a92afddcc201fc24f2c1d0543",
+    "url": "/static/media/sora-latin-ext-400-normal.5919e76a.woff2"
+  },
+  {
+    "revision": "40f332991081a9ba497e33df8381818e",
+    "url": "/static/media/sora-all-400-normal.40f33299.woff"
+  },
+  {
+    "revision": "ddd42916ed42bd1560d5ad15d2cb00ad",
+    "url": "/static/media/sora-latin-400-normal.ddd42916.woff2"
+  },
+  {
+    "revision": "53055e85d36853e1d30e34b8fb51e43f",
+    "url": "/index.html"
+  }
+];
